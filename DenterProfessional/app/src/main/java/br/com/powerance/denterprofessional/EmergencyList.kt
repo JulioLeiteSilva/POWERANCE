a3314d3d5e94a290d7134f881156b6ec91b14627
Fernando Martins <fj407161@gmail.com>
2023-05-03 22:47:23 -0300

    Implementação da Home Page e Recycler View

---
+++ DenterProfessional/app/src/main/java/br/com/powerance/denterprofessional/EmergencyList.kt
@@ -0,0 +1,11 @@
+package br.com.powerance.denterprofessional
+
+
+fun emergenciesList(): List<Emergency>{
+    return listOf(
+        Emergency("Messi careca", "(19)1111-2222",R.drawable.messi_careca,"Nova","03/05/2023, 21:32"),
+        Emergency("Russin do Molejo", "(19)1111-2222",R.drawable.messi_careca,"Nova","03/05/2023, 21:32"),
+        Emergency("Messi Bold", "(19)1111-2222",R.drawable.messi_careca,"Nova","03/05/2023, 21:32"),
+        Emergency("Messi Sem Cabelo", "(19)1111-2222",R.drawable.messi_careca,"Nova","03/05/2023, 21:32")
+    )
+}
