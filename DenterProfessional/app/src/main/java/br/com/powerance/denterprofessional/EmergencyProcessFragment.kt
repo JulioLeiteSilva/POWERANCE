913e6defabf395adf53f9b9a39ef0534ee320c8a
Fernando Martins <fj407161@gmail.com>
2023-06-15 04:34:37 -0300

    Pequenas Correções

--- DenterProfessional/app/src/main/java/br/com/powerance/denterprofessional/EmergencyProcessFragment.kt
+++ DenterProfessional/app/src/main/java/br/com/powerance/denterprofessional/EmergencyProcessFragment.kt
@@ -28,7 +28,7 @@ class EmergencyProcessFragment : Fragment() {
         val view = binding.root
 
         val emergencyData = (activity as? EmergencyActivity)?.emergency
-        val userAdress = (activity as? EmergencyActivity)?.userAddress
+        val userAdress = (activity as? EmergencyActivity)?.userAddress // Não utilizada agora
 
         binding.buttonTelefonar.setOnClickListener {
             if (emergencyData != null) {
@@ -45,7 +45,7 @@ class EmergencyProcessFragment : Fragment() {
                         val latitude = geoPoint.latitude
                         val longitude = geoPoint.longitude
                         if (userAdress != null) {
-                            irAteLocal(userAdress,latitude,longitude)
+                            irAteLocal(latitude,longitude)
                         }
                     } else {
                         // Documento não encontrado ou ocorreu um erro
@@ -55,8 +55,21 @@ class EmergencyProcessFragment : Fragment() {
             }
         }
 
-        binding.buttonReceberPaciente.setOnClickListener {
-            receberPaciente()
+        binding.buttonFinalizarEmergencia.setOnClickListener{
+            val documentId = emergencyData?.uid
+            val status = "finished"
+
+            if (documentId != null) {
+                db.collection("emergency").document(documentId)
+                    .update("status", status)
+                    .addOnSuccessListener {
+                        val intent = Intent(activity, MenuActivity::class.java)
+                        startActivity(intent)
+                    }
+                    .addOnFailureListener { exception ->
+                        // Lide com o erro ao atualizar o campo no banco de dados
+                    }
+            }
         }
 
         return view
@@ -92,29 +105,16 @@ class EmergencyProcessFragment : Fragment() {
             }
     }
     // A
-    private fun irAteLocal(enderecoPartida: String, latitudeDestino: Double, longitudeDestino: Double) {
+    private fun irAteLocal(latitudeDestino: Double, longitudeDestino: Double) {
 
-        val geocoder = context?.let { Geocoder(it) }
-        val addressList = geocoder?.getFromLocationName(enderecoPartida, 1) // Pegar localização do endereço do dentista
-        if (addressList != null) {
-            if (addressList.isNotEmpty()) {
-                val address = addressList[0]
-                val partidaLatitude = address.latitude
-                val partidaLongitude = address.longitude
+        val gmmIntentUri = Uri.parse("https://www.google.com/maps/dir/?api=1&destination=$latitudeDestino,$longitudeDestino&travelmode=driving")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
 
-                // Mapa Vai com Localização atual do Dentista
-                val gmmIntentUri = Uri.parse("https://www.google.com/maps/dir/?api=1&destination=$latitudeDestino,$longitudeDestino&travelmode=driving")
-                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-                mapIntent.setPackage("com.google.android.apps.maps")
-
-                if (mapIntent.resolveActivity(requireActivity().packageManager) != null) {
-                    startActivity(mapIntent)
-                } else {
-                    // Lide com a situação em que não há aplicativo de mapas instalado
-                }
-            } else {
-                // Lide com a situação em que o endereço de partida não pôde ser convertido em coordenadas geográficas
-            }
+        if (mapIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(mapIntent)
+        } else {
+            // Lide com a situação em que não há aplicativo de mapas instalado
         }
     }
 
